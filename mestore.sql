d7a6c2ae989bf1c2be2f1572dee2e352c9074345
Apriyanto <apri@github.com>
2018-03-02 15:35:33 +0700

    update database

--- mestore.sql
+++ mestore.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.6.6
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 12, 2018 at 06:21 PM
--- Server version: 10.1.29-MariaDB
--- PHP Version: 7.2.0
+-- Generation Time: Mar 02, 2018 at 09:25 AM
+-- Server version: 5.7.9
+-- PHP Version: 7.1.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -25,15 +23,43 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `balasan`
+-- Table structure for table `job`
 --
 
-CREATE TABLE `balasan` (
+CREATE TABLE `job` (
   `id` int(11) NOT NULL,
-  `id_komen` int(11) DEFAULT NULL,
-  `balasan` varchar(1000) DEFAULT NULL,
-  `time_balas` timestamp NULL DEFAULT NULL,
-  `id_us` int(11) DEFAULT NULL
+  `id_us` int(11) NOT NULL,
+  `subjek` varchar(225) NOT NULL,
+  `isi` text NOT NULL,
+  `tgl_post` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job_ampply`
+--
+
+CREATE TABLE `job_ampply` (
+  `id` int(11) NOT NULL,
+  `author` int(11) NOT NULL,
+  `id_us` int(11) NOT NULL,
+  `tgl_ampply` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id_job` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job_comment`
+--
+
+CREATE TABLE `job_comment` (
+  `id` int(11) NOT NULL,
+  `id_job` int(11) NOT NULL,
+  `id_us` int(11) NOT NULL,
+  `isi` varchar(10000) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -72,6 +98,24 @@ CREATE TABLE `komentar` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `pesan`
+--
+
+CREATE TABLE `pesan` (
+  `id` int(11) NOT NULL,
+  `pengirim` varchar(225) NOT NULL,
+  `isi` text NOT NULL,
+  `tgl_kirim` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `penerima` varchar(225) NOT NULL,
+  `read_penerima` tinyint(1) NOT NULL,
+  `read_pengirim` tinyint(1) NOT NULL,
+  `delete_penerima` tinyint(1) NOT NULL,
+  `delete_pengirim` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `produk`
 --
 
@@ -81,21 +125,22 @@ CREATE TABLE `produk` (
   `produk` varchar(225) DEFAULT NULL,
   `img` varchar(225) DEFAULT NULL,
   `harga` varchar(15) DEFAULT NULL,
-  `file` varchar(225) DEFAULT NULL,
-  `web_link` varchar(225) DEFAULT NULL,
-  `keterangan` text,
+  `link` varchar(225) DEFAULT NULL,
+  `demo` varchar(225) DEFAULT NULL,
+  `detail` text,
   `view` int(11) DEFAULT '0',
   `download` int(11) DEFAULT '0',
   `like` int(11) DEFAULT '0',
-  `tgl_simpan` timestamp NULL DEFAULT NULL
+  `tgl_post` timestamp NULL DEFAULT NULL,
+  `jenis` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `produk`
 --
 
-INSERT INTO `produk` (`id`, `id_kat`, `produk`, `img`, `harga`, `file`, `web_link`, `keterangan`, `view`, `download`, `like`, `tgl_simpan`) VALUES
-(1, 1, 'asfsdgsfg', NULL, '35456', NULL, NULL, NULL, 0, 0, 0, NULL);
+INSERT INTO `produk` (`id`, `id_kat`, `produk`, `img`, `harga`, `link`, `demo`, `detail`, `view`, `download`, `like`, `tgl_post`, `jenis`) VALUES
+(1, 1, 'asfsdgsfg', NULL, '35456', NULL, NULL, NULL, 0, 0, 0, NULL, '');
 
 -- --------------------------------------------------------
 
@@ -120,9 +165,21 @@ CREATE TABLE `user` (
 --
 
 --
--- Indexes for table `balasan`
+-- Indexes for table `job`
 --
-ALTER TABLE `balasan`
+ALTER TABLE `job`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `job_ampply`
+--
+ALTER TABLE `job_ampply`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `job_comment`
+--
+ALTER TABLE `job_comment`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -135,6 +192,12 @@ ALTER TABLE `kategori`
 -- Indexes for table `komentar`
 --
 ALTER TABLE `komentar`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pesan`
+--
+ALTER TABLE `pesan`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -154,36 +217,45 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT for table `balasan`
+-- AUTO_INCREMENT for table `job`
 --
-ALTER TABLE `balasan`
+ALTER TABLE `job`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
+--
+-- AUTO_INCREMENT for table `job_ampply`
+--
+ALTER TABLE `job_ampply`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `job_comment`
+--
+ALTER TABLE `job_comment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `kategori`
 --
 ALTER TABLE `kategori`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
 --
 -- AUTO_INCREMENT for table `komentar`
 --
 ALTER TABLE `komentar`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
+--
+-- AUTO_INCREMENT for table `pesan`
+--
+ALTER TABLE `pesan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
